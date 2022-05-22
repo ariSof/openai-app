02805cacf8a1a93d3848800ad9165a7303603747
ariSof <sofiaboubion@gmail.com>
2022-05-22 15:36:41 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import styles from "./App.css";
+
+
+import secret from './constant.js';
+const API_key = secret.OPENAI_SECRET;
+var postedResponse = "";
 
 function App() {
+  const [promptInput, setPromptInput] = useState("");
+  const [result, setResult] = useState("");
+  const [display, setDisplay] = useState([]);
+  
+  const addDisplayItem = (thePrompt, theResult) => {
+      const prevState = display;
+      setDisplay([...prevState, 'Prompt: '+ thePrompt + ' ___ Result:' + theResult]);
+  }
+
+  async function onSubmit(event) {
+    event.preventDefault();
+
+    const data = {
+      prompt: promptInput,
+      temperature: 0.5,
+      max_tokens: 64,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+     };
+
+    const auth = 'Bearer '+ API_key;
+    const response = await fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: auth,
+      },
+      body: JSON.stringify(data),
+   });
+    
+    const newData = await response.json();
+    const thePrompt = data.prompt;
+    const theResult = newData.choices[0].text;
+    postedResponse = '  Prompt: ' + data.prompt +   '     Response:' + newData.choices[0].text + postedResponse;
+   
+    setResult(postedResponse);
+    setPromptInput("");
+    addDisplayItem(thePrompt, theResult);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <main className={styles.main}>
+  
+      <h1>Enter a prompt:</h1>
+      <form  onSubmit={onSubmit}>
+        <textarea
+          type="text"
+          name="prompt"
+          value={promptInput}
+          onChange={(e) => setPromptInput(e.target.value)}
+        ></textarea>
+    
+        <input type="submit" value="Submit" />
+      </form>
+  
+      <div id="resultDiv" className={styles.result}>
+      
+        <ul>
+        {display.map((result, index) => <li key={index}> {result} </li>)}
+        </ul>
+      </div>  
+      </main>
     </div>
   );
 }
